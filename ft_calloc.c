f4e29506daf7cb462bbd14213d18e3dce3e0d798
Jauppp <127675629+Jauppp@users.noreply.github.com>
2023-11-10 16:31:22 +0100

    Add files via upload

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,23 @@
 /*   By: cdomet-d <cdomet-d@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 15:29:56 by cdomet-d          #+#    #+#             */
-/*   Updated: 2023/11/10 15:30:09 by cdomet-d         ###   ########lyon.fr   */
+/*   Updated: 2023/11/10 16:29:55 by cdomet-d         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_calloc(size_t nmemb, size_t size)
-{
+#include "libft.h"
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	// if (nmemb * size > MAX_INT)
+	// 	return (0);
+	arr = malloc((size) * (nmemb));
+	if (!arr)
+		return (arr);
+	ft_bzero(arr, nmemb);
+	return (arr);
 }
